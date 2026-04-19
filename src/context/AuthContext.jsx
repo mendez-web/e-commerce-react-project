40bf867a1e1fd381e836896fab27b4772616cb1b
Eugene Mendez <ecmendez1420@gmail.com>
2026-04-19 15:11:19 +0800

    Added Auth

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { AuthContext } from "./auth-context";
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const currentUserEmail = localStorage.getItem("currentUserEmail");
+    return currentUserEmail ? { email: currentUserEmail } : null;
+  });
+
+  function signUp(email, password) {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    if (users.find((u) => u.email === email)) {
+      return { success: false, error: "Email already exist" };
+    }
+    const newUser = { email, password };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email);
+
+    setUser({ email });
+    return { success: true };
+  }
+
+  function login(email, password) {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const user = users.find(
+      (u) => u.email === email && u.password === password,
+    );
+
+    if (!user) {
+      return { success: false, error: "Invalid Email Password" };
+    }
+
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
+
+    return { success: true };
+  }
+
+  function logout() {
+    localStorage.removeItem("currentUserEmail");
+    setUser(null);
+  }
+  return (
+    <AuthContext.Provider value={{ signUp, user, logout, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,111 @@
+import { useState, useContext } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../context/auth-context";
+import { useNavigate } from "react-router-dom";
+
 const Auth = () => {
-  return <div>Auth</div>;
+  const [mode, setMode] = useState("signup");
+  const [error, setError] = useState(null);
+  const { signUp, user, logout, login } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    let result;
+    if (mode === "signup") {
+      result = signUp(data.email, data.password);
+    } else {
+      result = login(data.email, data.password);
+    }
+
+    if (result.success) {
+      navigate("/");
+    } else {
+      setError(result.error);
+    }
+    console.log(result);
+  }
+
+  return (
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          {user && <p>User logged In: {user.email}</p>}
+          <button onClick={() => logout()}>logout</button>
+          <h1 className="page-title">
+            {mode === "signup" ? "Sign Up" : "LogIn"}
+          </h1>
+          <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+            {error && <div className="error-message">{error}</div>}
+            <div className="form-group">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-input"
+                id="email"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <span className="form-error">{errors.email.message}</span>
+              )}
+            </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "password must be at least 6 characters",
+                  },
+                  maxLength: {
+                    value: 12,
+                    message: "password must be less that 12 characters",
+                  },
+                })}
+                type="password"
+                className="form-input"
+                id="password"
+              />
+              {errors.password && (
+                <span className="form-error">{errors.password.message}</span>
+              )}
+            </div>
+            <button className="btn btn-primary btn-large">
+              {mode === "signup" ? "Sign Up" : "LogIn"}
+            </button>
+          </form>
+
+          <div className="auth-swith">
+            {mode === "signup" ? (
+              <p>
+                Already have an account?{" "}
+                <span className="auth-link" onClick={() => setMode("login")}>
+                  Login
+                </span>
+              </p>
+            ) : (
+              <p>
+                Already have an account?{" "}
+                <span className="auth-link" onClick={() => setMode("signup")}>
+                  Signup
+                </span>
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
